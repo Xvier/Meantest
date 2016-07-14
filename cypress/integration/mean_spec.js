eeb815c1c492acbb4e9560e06e76cc60d524ae16
Xvier <xviuda@xebia.com>
2016-07-14 20:15:35 +0200

    screenshot with cypress

--- cypress/integration/mean_spec.js
+++ cypress/integration/mean_spec.js
@@ -2,7 +2,8 @@
 describe('first test',function () {
     it('spec name', function () {
 
-        cy.visit('localhost:3000')
+        cy.visit('localhost:3000');
+        cy.screenshot("my-image");
        // cy.get('#query-btn').should('contain', 'Button')
     });
 });
