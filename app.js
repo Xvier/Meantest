4c8c0f52fb42d3eb7b54688f9f506d2c0f1e18cd
Xvier <xviuda@xebia.com>
2016-07-18 15:58:43 +0200

    added pug homepage

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var responseTime = require('response-time');
 
@@ -15,16 +15,18 @@ app.set('view engine', 'pug');
 app.use(responseTime());
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
 
 app.use(function(req, res, next) {
     res.status(404).send('Sorry cant find that!');
+    next();
 });
 
 
 app.listen(process.env.PORT || '3000', function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port ' + (process.env.PORT || '3000'));
 });
 
